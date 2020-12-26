e7277d5b6a7d5027779b67bd23f016a2d7e31739
nscando <nicolas.scandizzo@gmail.com>
2020-12-26 18:30:54 -0300

    enums - union - interface - monitor - integration

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
+const cors = require('cors')
 const { graphqlHTTP } = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
@@ -10,6 +11,7 @@ const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.PORT || 3000
+const isDev = process.env.NODE_ENV !== 'production'
 
 
 // schema
@@ -19,10 +21,13 @@ const typeDefs = readFileSync(
 )
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
+
+app.use(cors())
+
 app.use('/api', graphqlHTTP({
   schema: schema,
   rootValue: resolvers,
-  graphiql: true
+  graphiql: isDev
 }))
 
 app.listen(port, () => {
